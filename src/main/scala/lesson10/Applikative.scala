b9bf9b3129a5c290eca3964d999959b7a60d77ea
dmedser <dmedser.mephi@gmail.com>
2019-03-16 19:38:31 +0300

    Applikative

--- src/main/scala/lesson10/Applikative.scala
+++ src/main/scala/lesson10/Applikative.scala
@@ -2,20 +2,21 @@ package lesson10
 
 import cats.Monoid
 import lesson8.{ContraFunktor, Funktor}
-
 import scala.language.higherKinds
 
-trait Manoidal[F[_]] extends Funktor[F] {
+trait Manoidal[F[_]] {
   def unit: F[Unit]
 
   def product[A, B](fa: F[A], fb: F[B]): F[(A, B)]
 }
 
-trait Applikative[F[_]] extends Manoidal[F] {
+
+trait Applikative[F[_]] extends Funktor[F] with Manoidal[F] {
   def pure[A](a: A): F[A] = as(unit, a)
 
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) ⇒ C): F[C] = map(product(fa, fb))(ab ⇒ f(ab))
 }
+
 
 object Aplikative {
 
@@ -127,11 +128,7 @@ object Aplikative {
   implicit def someExpApplikative[F[_] : ContrApplikative]: Applikative[λ[α ⇒ F[α] ⇒ α]] = new Applikative[λ[α ⇒ F[α] ⇒ α]] {
     def unit: F[Unit] ⇒ Unit = _ ⇒ ()
 
-    def product[A, B](faa: F[A] ⇒ A, fbb: F[B] ⇒ B): F[(A, B)] ⇒ (A, B) = fab ⇒ {
-      val fa = implicitly[ContrApplikative[F]].map(fab) { case(a, _) ⇒ a}
-      val fb = implicitly[ContrApplikative[F]].map(fab) { case(_, b) ⇒ b}
-      (faa(fa), fbb(fb))
-    }
+    def product[A, B](faa: F[A] ⇒ A, fbb: F[B] ⇒ B): F[(A, B)] ⇒ (A, B) = ???
 
     def lift[A, B](f: A ⇒ B): (F[A] ⇒ A) ⇒ F[B] ⇒ B = faa ⇒ {
       fb ⇒ {
@@ -142,6 +139,7 @@ object Aplikative {
     }
   }
 }
+
 
 trait ContrApplikative[F[_]] extends ContraFunktor[F] with Manoidal[F] {
   def conquer[A]: F[A] = contramap[Unit, A](unit)((_: A) ⇒ ())
@@ -155,21 +153,16 @@ object ContrAplikative {
   // including M = 1 (any singleton)
   implicit def constContrApplikative[M : Monoid]: ContrApplikative[λ[α ⇒ M]] = new ContrApplikative[λ[α ⇒ M]] {
     import cats.syntax.semigroup._
-    def contralift[A, B](f: B ⇒ A): M ⇒ M = m ⇒ m
 
     def unit: M = implicitly[Monoid[M]].empty
 
     def product[A, B](fa: M, fb: M): M = fa |+| fb
 
-    def lift[A, B](f: A ⇒ B): M ⇒ M = m ⇒ m
+    def contralift[A, B](f: B ⇒ A): M ⇒ M = m ⇒ m
   }
 
 
   implicit def prodContrApplikative[F[_] : ContrApplikative, G[_] : ContrApplikative]: ContrApplikative[λ[α ⇒ (F[α], G[α])]] = new ContrApplikative[λ[α ⇒ (F[α], G[α])]] {
-    def contralift[A, B](f: B ⇒ A): ((F[A], G[A])) ⇒ (F[B], G[B]) = { case (fa, ga) ⇒
-      (implicitly[ContrApplikative[F]].contramap(fa)(f), implicitly[ContrApplikative[G]].contramap(ga)(f))
-    }
-
     def unit: (F[Unit], G[Unit]) = (implicitly[ContrApplikative[F]].unit, implicitly[ContrApplikative[G]].unit)
 
     def product[A, B](faga: (F[A], G[A]), fbgb: (F[B], G[B])): (F[(A, B)], G[(A, B)]) = {
@@ -180,8 +173,8 @@ object ContrAplikative {
       (implicitly[ContrApplikative[F]].product(fa, fb), implicitly[ContrApplikative[G]].product(ga, gb))
     }
 
-    def lift[A, B](f: A ⇒ B): ((F[A], G[A])) ⇒ (F[B], G[B]) = { case(fa, ga) ⇒
-      (implicitly[ContrApplikative[F]].map(fa)(f), implicitly[ContrApplikative[G]].map(ga)(f))
+    def contralift[A, B](f: B ⇒ A): ((F[A], G[A])) ⇒ (F[B], G[B]) = { case (fa, ga) ⇒
+      (implicitly[ContrApplikative[F]].contramap(fa)(f), implicitly[ContrApplikative[G]].contramap(ga)(f))
     }
   }
 
@@ -191,25 +184,18 @@ object ContrAplikative {
 
 
   implicit def compContrApplikative[F[_] : Applikative, G[_] : ContrApplikative]: ContrApplikative[λ[α ⇒ F[G[α]]]] = new ContrApplikative[λ[α ⇒ F[G[α]]]] {
-    def contralift[A, B](f: B ⇒ A): F[G[A]] ⇒ F[G[B]] =
-      fga ⇒ implicitly[Applikative[F]].map(fga)(ga ⇒ implicitly[ContrApplikative[G]].contramap(ga)(f))
-
     def unit: F[G[Unit]] = implicitly[Applikative[F]].pure(implicitly[ContrApplikative[G]].unit)
 
     def product[A, B](fga: F[G[A]], fgb: F[G[B]]): F[G[(A, B)]] =
       implicitly[Applikative[F]].map2(fga, fgb) { case (ga, gb) ⇒ implicitly[ContrApplikative[G]].product(ga, gb)}
 
-    def lift[A, B](f: A ⇒ B): F[G[A]] ⇒ F[G[B]] = fga ⇒ implicitly[Applikative[F]].map(fga)(ga ⇒ implicitly[ContrApplikative[G]].map(ga)(f))
+    def contralift[A, B](f: B ⇒ A): F[G[A]] ⇒ F[G[B]] =
+      fga ⇒ implicitly[Applikative[F]].map(fga)(ga ⇒ implicitly[ContrApplikative[G]].contramap(ga)(f))
+
   }
 
 
   implicit def expContrApplikative[F[_] : Funktor, G[_] : ContrApplikative]: ContrApplikative[λ[α ⇒ F[α] ⇒ G[α]]] = new ContrApplikative[λ[α ⇒ F[α] ⇒ G[α]]] {
-    def contralift[A, B](f: B ⇒ A): (F[A] ⇒ G[A]) ⇒ F[B] ⇒ G[B] = faga ⇒ { fb ⇒
-      val fa = implicitly[Funktor[F]].map(fb)(f)
-      val ga = faga(fa)
-      implicitly[ContrApplikative[G]].contramap(ga)(f)
-    }
-
     def unit: F[Unit] ⇒ G[Unit] = _ ⇒ implicitly[ContrApplikative[G]].unit
 
     def product[A, B](faga: F[A] ⇒ G[A], fbgb: F[B] ⇒ G[B]): F[(A, B)] ⇒ G[(A, B)] = fab ⇒ {
@@ -220,12 +206,10 @@ object ContrAplikative {
       implicitly[ContrApplikative[G]].product(ga, gb)
     }
 
-    def lift[A, B](f: A ⇒ B): (F[A] ⇒ G[A]) ⇒ F[B] ⇒ G[B] = faga ⇒ {
-      fb ⇒ {
-        val fa = ???
-        val ga = faga(fa)
-        implicitly[ContrApplikative[G]].map(ga)(f)
-      }
+    def contralift[A, B](f: B ⇒ A): (F[A] ⇒ G[A]) ⇒ F[B] ⇒ G[B] = faga ⇒ { fb ⇒
+      val fa = implicitly[Funktor[F]].map(fb)(f)
+      val ga = faga(fa)
+      implicitly[ContrApplikative[G]].contramap(ga)(f)
     }
   }
 }
