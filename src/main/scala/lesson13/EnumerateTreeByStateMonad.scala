1a2d37b1c62f147e46d244f698cf4cbbb0ecd661
dmedser <dmedser.mephi@gmail.com>
2019-05-01 23:24:35 +0300

    lesson13 CountPositiveNumbersByStateMonad

--- src/main/scala/lesson13/EnumerateTreeByStateMonad.scala
+++ src/main/scala/lesson13/EnumerateTreeByStateMonad.scala
@@ -53,3 +53,27 @@ object EnumerateTreeByStateMonad extends App {
 
   println(enumerate(tree))
 }
+
+object CountPositiveNumbersByStateMonad extends App {
+
+  import cats.Monoid
+  import cats.instances.int._
+  import cats.syntax.semigroup._
+
+  def countByPredicate[A : Monoid](list: List[A], p: A => Boolean): State[Int, A] =
+    list match {
+      case x :: xs if p(x) =>
+        for {
+          _ <- State.modify[Int](_ + 1)
+          restAppropriateXs <- countByPredicate(xs, p)
+        } yield x |+| restAppropriateXs
+      case x :: xs if !p(x) =>
+        countByPredicate(xs, p)
+      case Nil =>
+        State.empty
+    }
+
+  val numbers = List(-1, -2, 0, 1, 2, -3, 4, 0, 0, 10)
+
+  println(countByPredicate[Int](numbers, _ > 0).run(0).value)
+}
